a886959a2e590fb3f16ed9be82396b955297e819
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-08-05 00:44:59 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # msxrom2vhd
-Convert MSX rom files into autolaunch VHD images.
+
+Convert MSX rom files into autolaunch VHD images for MiSTer.
+
+
+Installation
+============
+
+You need `libguestfs` installed in your system.
+
+```
+make clean && make
+```
+
+
+Usage
+=====
+
+```
+Usage: ./msxrom2vhd <input.rom> <output.vhd>
+```
+
+Acknowledgements
+================
+
+This software contains binary files gathered from https://github.com/MiSTer-devel/MSX_MiSTer/tree/master/Utils/sdcreate.zip as well as `execrom` from https://sourceforge.net/projects/execrom/files/ExecROM%20binaries/1.2c/.
+
---
+++ main.c
@@ -0,0 +1,87 @@
+#include <guestfs.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ocmbiosdat.h"
+
+#include "msxdos2sys.h"
+#include "nextorsys.h"
+#include "command2com.h"
+
+#include "execromcom.h"
+#include "autoexecbat.h"
+
+const size_t fs_size = 159040;
+
+void main(int argc, char *argv[]) {
+
+	size_t system_size = (static_OCM_BIOS_DAT_len
+			      + static_MSXDOS2_SYS_len
+			      + static_NEXTOR_SYS_len
+			      + static_COMMAND2_COM_len
+			      + static_execrom_com_len
+			      + static_AUTOEXEC_BAT_len);
+
+	if (argc != 3) {
+		printf("Usage: %s <input.rom> <output.vhd>\n", argv[0]);
+		exit(1);
+	}
+
+	struct stat st;
+	stat(argv[1], &st);
+	size_t rom_size = st.st_size;
+
+	printf("filesystem: %d\n", fs_size);
+	printf("system: %d\n", system_size);
+	printf("user: %d\n", rom_size);
+
+	guestfs_h *g = guestfs_create ();
+	guestfs_disk_create (g, argv[2], "raw", fs_size + system_size + rom_size, -1);
+	guestfs_add_drive_opts (g, argv[2],
+			        GUESTFS_ADD_DRIVE_OPTS_FORMAT, "raw",
+				-1);
+	guestfs_launch (g);
+	guestfs_part_disk (g, "/dev/sda", "mbr");
+	guestfs_mkfs(g, "fat", "/dev/sda1");
+	guestfs_mount (g, "/dev/sda1", "/");
+
+	/* BIOS */
+	printf("Copying BIOS...\n");
+	guestfs_write (g, "/OCM-BIOS.DAT",
+		       static_OCM_BIOS_DAT,
+		       static_OCM_BIOS_DAT_len);
+
+	/* OS */
+	printf("Copying OS...\n");
+	guestfs_write (g, "/MSXDOS2.SYS",
+		       static_MSXDOS2_SYS,
+		       static_MSXDOS2_SYS_len);
+
+	guestfs_write (g, "/NEXTOR.SYS",
+		       static_NEXTOR_SYS,
+		       static_NEXTOR_SYS_len);
+
+	guestfs_write (g, "/COMMAND2.COM",
+		       static_COMMAND2_COM,
+		       static_COMMAND2_COM_len);
+
+	/* ROM LOADER */
+	printf("Copying loader...\n");
+	guestfs_write (g, "/EXECROM.COM",
+		       static_execrom_com,
+		       static_execrom_com_len);
+
+	guestfs_write (g, "/AUTOEXEC.BAT",
+		       static_AUTOEXEC_BAT,
+		       static_AUTOEXEC_BAT_len);
+
+	/* ROM */
+	printf("Copying rom...\n");
+	guestfs_upload (g, argv[1], "/USER.ROM");
+
+	guestfs_umount (g, "/");
+	guestfs_shutdown (g);
+	guestfs_close (g);
+}
