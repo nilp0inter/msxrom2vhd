bf01e4c89e263afa7a810a005710c1220cb5e19f
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-01-14 11:16:01 +0100

    feature: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "msxrom2vhd";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages = rec {
+          msxrom2vhd = with pkgs; stdenv.mkDerivation {
+            name = "msxrom2vhd";
+            version = "1.1";
+            src = ./.;
+            nativeBuildInputs = [ automake autoconf xxd ];
+            buildInputs = [ libguestfs ];
+            preConfigure = ''
+              ./autoconf.sh
+            '';
+          };
+        };
+        defaultPackage = packages.msxrom2vhd;
+#        apps.msxrom2vhd = flake-utils.lib.mkApp { drv = packages.msxrom2vhd; };
+#        apps.default = apps.msxrom2vhd;
+      }
+    );
+}
